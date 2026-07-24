e68844c4247e5ec5d4dcfe884c1225a145ecd44d
Grok Build Max <gmax@local>
2026-07-24 08:57:22 +0800

    Add live $GMAX mint 5mB6hVACwaXiLXNXLe5En3KzCxuEyYcfveMGWT9wj4sh

---
+++ metadata.js
@@ -0,0 +1,7 @@
+window.GMAX = {
+  "mint": "5mB6hVACwaXiLXNXLe5En3KzCxuEyYcfveMGWT9wj4sh",
+  "pumpUrl": "https://pump.fun/coin/5mB6hVACwaXiLXNXLe5En3KzCxuEyYcfveMGWT9wj4sh",
+  "signature": "4LCqsHvNmUaaGDqYLSQYhgoFs8hjFxX8KfZyPzTWDGqkTDiFzJfVQ7aAh9mUKA1zb5JDMjiQ4YTf9NP5dLo9isWE",
+  "twitter": "https://x.com/JohnnnnHh",
+  "website": "https://zhaohh0706.github.io/gmax-solana/"
+};
